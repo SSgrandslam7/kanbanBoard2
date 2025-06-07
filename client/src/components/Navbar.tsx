abd64b41f3146ddd80b2afc62263b251f0118896
Stephen Schier <stephenjschier@gmail.com>
2025-06-07 16:58:09 -0500

    fixed logout function

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
-  const [ loginCheck, setLoginCheck ] = useState(false);
+  const [loginCheck, setLoginCheck] = useState(false);
+  const navigate = useNavigate();
 
   const checkLogin = () => {
-    if(auth.loggedIn()) {
+    if (auth.loggedIn()) {
       setLoginCheck(true);
     }
   };
 
   useEffect(() => {
-    console.log(loginCheck);
     checkLogin();
-  }, [loginCheck])
+  }, [loginCheck]);
+
+  const handleLogout = () => {
+    auth.logout();
+    navigate('/login');
+  };
 
   return (
     <div className='nav'>
@@ -22,24 +27,22 @@ const Navbar = () => {
         <Link to='/'>Krazy Kanban Board</Link>
       </div>
       <ul>
-      {
-        !loginCheck ? (
-          <li className='nav-item'>
-            <button type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </li>
-        ) : (
-          <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
-          </li>
-        )
-      }
+        {
+          !loginCheck ? (
+            <li className='nav-item'>
+              <button type='button'>
+                <Link to='/login'>Login</Link>
+              </button>
+            </li>
+          ) : (
+            <li className='nav-item'>
+              <button type='button' onClick={handleLogout}>Logout</button>
+            </li>
+          )
+        }
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
