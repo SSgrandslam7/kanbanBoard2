a9fda2ef5d6899010a719cfc275476e5a6d4c362
Stephen Schier <stephenjschier@gmail.com>
2025-06-07 00:45:04 -0500

    initial utils/auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,29 +2,38 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode(token) : null;
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
-  }
-  
-  isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
 
-  getToken(): string {
-    // TODO: return the token
+  isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<JwtPayload & { exp: number }>(token);
+      if (!decoded.exp) return true;
+      return decoded.exp * 1000 < Date.now();
+    } catch (err) {
+      console.error("Token expiration check failed:", err);
+      return true;
+    }
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('id_token');
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login');
   }
 }
 
