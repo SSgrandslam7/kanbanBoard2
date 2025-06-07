1e2627e223d40de45a87656954eeed789d6d8e12
Stephen Schier <stephenjschier@gmail.com>
2025-06-07 15:33:56 -0500

    attempting to populate tickets

--- server/src/controllers/ticket-controller.ts
+++ server/src/controllers/ticket-controller.ts
@@ -14,6 +14,7 @@ export const getAllTickets = async (_req: Request, res: Response) => {
         },
       ],
     });
+    console.log('Fetched tickets:', tickets);
     res.json(tickets);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
